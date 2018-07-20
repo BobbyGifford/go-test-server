9125aabaf84e00d0befa8d3fa76c13783cef1051
Bobby <bobby077joe@gmail.com>
2018-07-20 12:14:19 -0700

    Added unmarshal

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"fmt"
 	"encoding/json"
+	"io/ioutil"
 )
 
 type person struct {
@@ -46,6 +47,32 @@ func marshalTest(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
+func unMarshalTest (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Read body first
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Unmarshal and send response back
+	var data person
+	err = json.Unmarshal(b, &data)
+		if err != nil{
+			log.Println(err)
+			return
+		}
+	output, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	w.Write(output)
+}
+
 func encodeTest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application-json")
 	p1 := person{
@@ -81,6 +108,7 @@ func main() {
 	http.HandleFunc("/handle/", urlValue)
 	http.HandleFunc("/jsontest/", marshalTest)
 	http.HandleFunc("/encodetest/", encodeTest)
+	http.HandleFunc("/unmarshaltest/", unMarshalTest)
 	http.HandleFunc("/decodetest/", decodeTest)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
